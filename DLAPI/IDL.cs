c2cb04ef1da85ca93fe930819e94dfd77d4cedc5
efrati.amar <efrati.amar@gmail.com>
2020-12-24 17:29:25 +0200

    put using DO; in remark to force writing DO.Person etc.

--- DLAPI/IDL.cs
+++ DLAPI/IDL.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-using DO;
+//using DO;
 
 namespace DLAPI
 {
@@ -13,31 +13,31 @@ namespace DLAPI
     public interface IDL
     {
         #region Person
-        IEnumerable<Person> GetAllPersons();
-        IEnumerable<Person> GetAllPersonsBy(Predicate<Person> predicate);
-        Person GetPerson(int id);
-        void AddPerson(Person person);
-        void UpdatePerson(Person person);
-        void UpdatePerson(int id, Action<Person> update); //method that knows to updt specific fields in Person
+        IEnumerable<DO.Person> GetAllPersons();
+        IEnumerable<DO.Person> GetAllPersonsBy(Predicate<DO.Person> predicate);
+        DO.Person GetPerson(int id);
+        void AddPerson(DO.Person person);
+        void UpdatePerson(DO.Person person);
+        void UpdatePerson(int id, Action<DO.Person> update); //method that knows to updt specific fields in Person
         void DeletePerson(int id);
         #endregion
 
         #region Student
-        Student GetStudent(int id);
+        DO.Student GetStudent(int id);
         IEnumerable<DO.Student> GetAllStudents();
         IEnumerable<object> GetStudentListWithSelectedFields(Func<DO.Student, object> generate);
-        void AddStudent(Student student);
-        void UpdateStudent(Student student);
-        void UpdateStudent(int id, Action<Student> update); //method that knows to updt specific fields in Student
+        void AddStudent(DO.Student student);
+        void UpdateStudent(DO.Student student);
+        void UpdateStudent(int id, Action<DO.Student> update); //method that knows to updt specific fields in Student
         void DeleteStudent(int id); // removes only Student, does not remove the appropriate Person...
         #endregion
 
         #region StudentInCourse
-        IEnumerable<StudentInCourse> GetStudentInCourseList(Predicate<StudentInCourse> predicate);
+        IEnumerable<DO.StudentInCourse> GetStudentInCourseList(Predicate<DO.StudentInCourse> predicate);
         #endregion
 
         #region Course
-        Course GetCourse(int id);
+        DO.Course GetCourse(int id);
         #endregion
     }
 }
